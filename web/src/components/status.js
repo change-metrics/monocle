a1dde6722c6386c5acee7d82b223fc2ace278a94
Fabien Boucher <fabien.dot.boucher@gmail.com>
2019-12-18 10:43:14 +0100

    web: factorize histos

--- web/src/components/status.js
+++ web/src/components/status.js
@@ -6,27 +6,23 @@ import Card from 'react-bootstrap/Card'
 
 import { Bar } from 'react-chartjs-2';
 
-class AllEventsHisto extends React.Component {
-  componentDidMount() {
-    this.props.query({
-      'org': 'kubernetes',
-      'name': 'events_histo',
-      'interval': '31d',
-      'graph_type': 'all_events',
-    })
+class BaseEventsHisto extends React.Component {
+  prepare_data_set(data, title) {
+    const dataset = {
+      labels: data.map(x => x.key_as_string),
+      datasets: [
+        {
+          label: title,
+          data: data.map(x => x.doc_count)
+        }
+      ]
+    }
+    return dataset
   }
-  render() {
-    if (!this.props.all_events_loading) {
-      console.log(this.props.all_events_result)
-      const data = {
-        labels: this.props.all_events_result[2].map(x => x.key_as_string),
-        datasets: [
-          {
-            label: 'Events histo',
-            data: this.props.all_events_result[2].map(x => x.doc_count)
-          }
-        ]
-      };
+  render_component(graph_type, title) {
+    if (!this.props[graph_type + '_loading']) {
+      const data = this.prepare_data_set(
+        this.props[graph_type + '_result'][2], title)
       return (
         <Row>
           <Col md={{ span: 8, offset: 2 }}>
@@ -60,58 +56,34 @@ class AllEventsHisto extends React.Component {
   }
 }
 
-class CloseEventsHisto extends React.Component {
+class AllEventsHisto extends BaseEventsHisto {
   componentDidMount() {
     this.props.query({
       'org': 'kubernetes',
       'name': 'events_histo',
-      'interval': '31d',
+      'interval': '15d',
+      'gte': '2019-07-01',
+      'graph_type': 'all_events',
+    })
+  }
+  render() {
+    return this.render_component('all_events', 'All events histogram')
+  }
+}
+
+class CloseEventsHisto extends BaseEventsHisto {
+  componentDidMount() {
+    this.props.query({
+      'org': 'kubernetes',
+      'name': 'events_histo',
+      'interval': '15d',
+      'gte': '2019-07-01',
       'type': 'PRClosedEvent',
       'graph_type': 'close_events',
     })
   }
   render() {
-    if (!this.props.close_events_loading) {
-      console.log(this.props.close_events_result)
-      const data = {
-        labels: this.props.close_events_result[2].map(x => x.key_as_string),
-        datasets: [
-          {
-            label: 'Events histo',
-            data: this.props.close_events_result[2].map(x => x.doc_count)
-          }
-        ]
-      };
-      return (
-        <Row>
-          <Col md={{ span: 8, offset: 2 }}>
-            <Card>
-              <Card.Body>
-                <Bar
-                  data={data}
-                  width={100}
-                  height={50}
-                />
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      )
-    } else {
-      return (
-        <Row>
-          <Col md={{ span: 8, offset: 2 }}>
-            <Card>
-              <Card.Body>
-                <h1>
-                  loading
-                </h1>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      )
-    }
+    return this.render_component('close_events', 'Pull Request close events histogram')
   }
 }
 
