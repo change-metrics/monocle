ba6fed713373328e3ac08decf9bfbf394fa979cc
Fabien Boucher <fabien.dot.boucher@gmail.com>
2020-02-26 08:54:37 +0100

    Move TopEventsTable component in a separate file

--- web/src/components/info.js
+++ web/src/components/info.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
-import Table from 'react-bootstrap/Table'
 import ListGroup from 'react-bootstrap/ListGroup'
 import {
   ChangeLifeCycleEventsHisto,
   ChangeReviewEventsHisto,
 } from './histo'
+import {
+  TopEventsTable
+} from './top'
 
 class InfoEvents extends React.Component {
   componentDidUpdate() {
@@ -217,41 +219,6 @@ class ChangesReviewStats extends React.Component {
   }
 }
 
-
-class TopEventsTable extends React.Component {
-  render() {
-    return (
-      <Row>
-        <Col>
-          <Card>
-            <Card.Header>
-              <Card.Title>{this.props.title}</Card.Title>
-            </Card.Header>
-            <Card.Body>
-              <Table striped responsive bordered hover>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>ID</th>
-                    <th>count</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.props.data.map((x, index) =>
-                    <tr key={index}>
-                      <td>{index}</td>
-                      <td>{x.key}</td>
-                      <td>{x.doc_count}</td>
-                    </tr>)}
-                </tbody>
-              </Table>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    )
-  }
-}
 
 class MostActiveAuthorsStats extends React.Component {
   componentDidUpdate() {
--- web/src/components/top.js
+++ web/src/components/top.js
@@ -6,92 +6,41 @@ import Card from 'react-bootstrap/Card'
 import Table from 'react-bootstrap/Table'
 
 
-class BaseTopEventsAuthor extends React.Component {
-  render_component(graph_type, title) {
-    if (!this.props[graph_type + '_loading']) {
-      const data = this.props[graph_type + '_result'][2]
-      return (
-        <Row>
-          <Col>
-            <Card>
-              <Card.Body>
-                <Table striped responsive bordered hover>
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>ID</th>
-                      <th>Review count</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.map((x, index) =>
-                      <tr key={index}>
-                        <td>{index}</td>
-                        <td>{x.key}</td>
-                        <td>{x.doc_count}</td>
-                      </tr>)}
-                  </tbody>
-                </Table>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      )
-    } else {
-      return (
-        <Row>
-          <Col>
-            <Card>
-              <Card.Body>
-                <h1>
-                  loading
-                </h1>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      )
-    }
-  }
-}
-
-class TopReviewers extends BaseTopEventsAuthor {
-  componentDidUpdate() {
-    if (this.props.filter_loaded_from_url && !this.props.top_reviewers_result) {
-      this.props.query({
-        'repository': this.props.filter_repository,
-        'name': 'events_top_authors',
-        'gte': this.props.filter_gte,
-        'lte': this.props.filter_lte,
-        'type': 'ChangeReviewedEvent',
-        'graph_type': 'top_reviewers',
-      })
-    }
-  }
+class TopEventsTable extends React.Component {
   render() {
-    return this.render_component('top_reviewers', 'Top reviewers')
-  }
-}
-
-class TopCreators extends BaseTopEventsAuthor {
-  componentDidUpdate() {
-    if (this.props.filter_loaded_from_url && !this.props.top_creators_result) {
-      this.props.query({
-        'repository': this.props.filter_repository,
-        'name': 'events_top_authors',
-        'gte': this.props.filter_gte,
-        'lte': this.props.filter_lte,
-        'type': 'ChangeCreatedEvent',
-        'graph_type': 'top_creators',
-      })
-    }
-  }
-  render() {
-    return this.render_component('top_creators', 'Top authors')
+    return (
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header>
+              <Card.Title>{this.props.title}</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Table striped responsive bordered hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>ID</th>
+                    <th>count</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.props.data.map((x, index) =>
+                    <tr key={index}>
+                      <td>{index}</td>
+                      <td>{x.key}</td>
+                      <td>{x.doc_count}</td>
+                    </tr>)}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    )
   }
 }
 
 export {
-  TopReviewers,
-  TopCreators,
+  TopEventsTable,
 }
