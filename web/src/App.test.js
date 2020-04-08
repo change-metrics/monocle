57a7f2f8bd829e212a4ad37df28b308acfa59cea
Frederic Lepied <flepied@gmail.com>
2020-04-08 08:50:02 +0200

    README: explain how to setup git hooks
    - README.md: describe how to setup pre-push and pre-commit git hooks
    - provide the git hooks for the project in contrib/
     - pre-commit: run black to reformat code
     - pre-push: run tox for python code and npm test and lint for web code
    - web: fix npm test
    - web: create .env with SKIP_PREFLIGHT_CHECK=true as preflight checks were failing

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { createMyStore } from './store'
 import App from './App'
+import { MemoryRouter } from 'react-router-dom'
+import {
+  toBeInTheDocument,
+  toHaveClass
+} from '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+expect.extend({ toBeInTheDocument, toHaveClass })
+
+test('renders Monocle link', () => {
+  const store = createMyStore()
+  const { getByText } = render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={['/index']}>
+        <App />
+      </MemoryRouter>
+    </Provider>
+  )
+  const linkElement = getByText(/Monocle/i)
   expect(linkElement).toBeInTheDocument()
+  expect(linkElement).toHaveClass('navbar-brand')
 })
