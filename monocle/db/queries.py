6c55b7615a16eddcfdd6c82b482114302c8dbd00
Fabien Boucher <fabien.dot.boucher@gmail.com>
2020-04-12 17:00:45 +0200

    queries: _event_top add paginations
    Terms aggregation does not support pagination. Thus we implement
    it outside of Elasticsearch. To avoid going over the whole dataset
    we limit the search to the 1000 highest count.
    
    The average and mediane are computed on up to 1000 buckets. And the
    pagination allow to move into those 1000 buckets as well.

--- monocle/db/queries.py
+++ monocle/db/queries.py
@@ -197,11 +197,7 @@ def _events_top(es, index, repository_fullname, field, params):
     body = {
         "aggs": {
             "agg1": {
-                "terms": {
-                    "field": field,
-                    "size": params['size'],
-                    "order": {"_count": "desc"},
-                }
+                "terms": {"field": field, "size": 1000, "order": {"_count": "desc"},}
             }
         },
         "size": 0,
@@ -211,11 +207,15 @@ def _events_top(es, index, repository_fullname, field, params):
     count_series = [b['doc_count'] for b in data['aggregations']['agg1']['buckets']]
     count_avg = statistics.mean(count_series) if count_series else 0
     count_median = statistics.median(sorted(count_series)) if count_series else 0
+    _from = params['from']
+    _to = params['from'] + params['size']
+    buckets = data['aggregations']['agg1']['buckets']
     return {
-        'items': data['aggregations']['agg1']['buckets'],
+        'items': buckets[_from:_to],
         'count_avg': count_avg,
         'count_median': count_median,
-        'total': data['hits']['total'],
+        'total': len(buckets),
+        'total_hits': data['hits']['total'],
     }
 
 
