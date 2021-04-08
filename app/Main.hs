3731016e142f9810a8527a2e9c3109eb2c442745
Tristan Cacqueray <tdecacqu@redhat.com>
2021-04-08 16:33:32 +0000

    Initial summon

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import Lentille (someFunc)
+
+
+main :: IO ()
+main = someFunc
---
+++ src/Lentille.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 Monocle authors
+SPDX-License-Identifier: AGPL-3.0-only
+Maintainer: Monocle authors <fboucher@redhat.com>
+
+See README for more info
+-}
+
+module Lentille
+       ( someFunc
+       ) where
+
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
