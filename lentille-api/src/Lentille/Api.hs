13209da7839b40889d7f796036e86b08762d66f8
Tristan Cacqueray <tdecacqu@redhat.com>
2021-04-15 14:36:02 +0000

    Add servant-openapi

--- lentille-api/src/Lentille/Api.hs
+++ lentille-api/src/Lentille/Api.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
@@ -5,6 +6,11 @@
 
 module Lentille.Api (app) where
 
+#ifdef USE_OPENAPI
+import Data.OpenApi hiding (server, Server) -- (OpenApi, ((.~)))
+import Servant.OpenApi (toOpenApi)
+import Control.Lens.Operators
+#endif
 import Data.Time (UTCTime, getCurrentTime)
 import Network.Wai (Application)
 import Relude
@@ -13,15 +19,20 @@ import Servant.API
 
 type ReqIndex = QueryParam "index" Text
 
-type TrackerAPI =
+type TrackerApi =
   "tracker_data" :> ReqIndex :> Get '[JSON] UTCTime
     :<|> "tracker_data" :> "commit" :> ReqIndex :> QueryParam "date" UTCTime :> Post '[PlainText] Text
 
-type ChangeAPI = "change_data" :> Get '[JSON] Text
+type ChangeApi = "change_data" :> Get '[JSON] Text
 
-type MonocleAPI = "api" :> "1" :> (TrackerAPI :<|> ChangeAPI)
+type MonocleApi = "api" :> "1" :> (TrackerApi :<|> ChangeApi)
 
-server :: IORef UTCTime -> Server MonocleAPI
+monocleApi :: Proxy MonocleApi
+monocleApi = Proxy
+
+type Db = IORef UTCTime
+
+server :: Db -> Server MonocleApi
 server db = tdApp :<|> changeApp
   where
     changeApp = changeGet
@@ -41,12 +52,34 @@ server db = tdApp :<|> changeApp
       pure "Commited"
     tdPost _ _ = error "Missing arguments"
 
+#ifdef USE_OPENAPI
+type SwaggerAPI = "swagger.json" :> Get '[JSON] OpenApi
+
+type MonocleWithOpenApi = SwaggerAPI :<|> MonocleApi
+
+monocleSwagger :: OpenApi
+monocleSwagger =
+  toOpenApi monocleApi
+  & info.title .~ "Monocle API"
+  & info.version .~ "2"
+  & info.description ?~ "Generated swagger API"
+
+finalApi :: Proxy MonocleWithOpenApi
+finalApi = Proxy
+
+finalServer :: Db -> Server MonocleWithOpenApi
+finalServer db = return monocleSwagger :<|> server db
+#else
+finalApi :: Proxy MonocleApi
+finalApi = monocleApi
+
+finalServer :: Db -> Server MonocleApi
+finalServer = server
+#endif
+
 app :: MonadIO m => m Application
 app = do
   now <- liftIO getCurrentTime
   db <- newIORef now
   putTextLn "Serving api"
-  pure $ serve api (server db)
-  where
-    api :: Proxy MonocleAPI
-    api = Proxy
+  pure $ serve finalApi (finalServer db)
