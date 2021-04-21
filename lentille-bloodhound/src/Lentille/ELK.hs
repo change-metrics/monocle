066f185521fc2ff2371cdac28f16146385149a70
Tristan Cacqueray <tdecacqu@redhat.com>
2021-04-21 20:13:35 +0000

    [bloodhound] Add patchCopyTaskData migration procedure
    This change adds a migration procedure to copy the taskdata from
    Change document to the other one.

--- lentille-bloodhound/src/Lentille/ELK.hs
+++ lentille-bloodhound/src/Lentille/ELK.hs
@@ -13,9 +13,6 @@ import Data.Aeson
   ( FromJSON (..),
     ToJSON (..),
     Value (..),
-    defaultOptions,
-    genericParseJSON,
-    genericToJSON,
     object,
     (.:),
     (.=),
@@ -23,7 +20,8 @@ import Data.Aeson
 import qualified Data.Text as T
 import qualified Data.Vector as V
 import qualified Database.Bloodhound as BH
-import Network.HTTP.Client (defaultManagerSettings, responseBody, responseStatus)
+import Lentille (TaskData (..))
+import Network.HTTP.Client (Response, defaultManagerSettings, responseBody, responseStatus)
 import Network.HTTP.Types.Status (Status (..))
 import Relude
 
@@ -36,23 +34,126 @@ instance ToJSON TaskDataMapping where
           .= object ["keywords" .= object ["type" .= ("keyword" :: Text)]]
       ]
 
-data TaskData = TaskData
-  { tdId :: Int,
-    url :: Text,
-    keywords :: [Text]
-  }
-  deriving stock (Eq, Generic, Show)
-
-instance ToJSON TaskData where
-  toJSON = genericToJSON defaultOptions
-
-instance FromJSON TaskData where
-  parseJSON = genericParseJSON defaultOptions
-
 getIndices :: (MonadThrow m, BH.MonadBH m) => m [Text]
 getIndices = do
   indices <- BH.listIndices
   pure . filter (T.isPrefixOf "monocle.") . map (\(BH.IndexName name) -> name) $ indices
+
+-- A document record containing tasks data
+data ChangeTD = ChangeTD
+  { changeTDUrl :: Text,
+    changeTDData :: [TaskData]
+  }
+  deriving stock (Eq, Show)
+
+instance FromJSON ChangeTD where
+  parseJSON (Object v) = do
+    url <- v .: "url"
+    -- This decode tasks_data as either a single item, or a list of items
+    tds <- v .: "tasks_data"
+    case tds of
+      td@(Object _) -> do
+        td' <- parseJSON td
+        pure $ ChangeTD url [td']
+      tds'@(Array _) -> ChangeTD url <$> parseJSON tds'
+      _ -> empty
+  parseJSON _ = empty
+
+-- A simpler document record without task data
+data ChangeNoTD = ChangeNoTD
+  { changeNoTDUrl :: Text,
+    changeNoTDType :: Text
+  }
+  deriving stock (Eq, Show)
+
+instance FromJSON ChangeNoTD where
+  parseJSON (Object v) = ChangeNoTD <$> v .: "url" <*> v .: "type"
+  parseJSON _ = empty
+
+-- | Helper func to check if a response is ok
+isRespOk :: Response a -> Bool
+isRespOk resp = case responseStatus resp of
+  Status code _ | code >= 200 && code <= 300 -> True
+  _ -> False
+
+-- | Helper search func that can be replaced by a scanSearch
+simpleSearch :: (FromJSON a, MonadThrow m, BH.MonadBH m) => BH.IndexName -> BH.Search -> m [BH.Hit a]
+simpleSearch indexName search = do
+  rawResp <- BH.searchByIndex indexName search
+  resp <- BH.parseEsResponse rawResp
+  case resp of
+    Left e -> error (show e)
+    Right x -> pure (BH.hits (BH.searchHits x))
+
+-- | A function to add task data to other events
+patchCopyTaskData :: (MonadThrow m, BH.MonadBH m) => BH.IndexName -> m ()
+patchCopyTaskData indexName = do
+  tds <- getTaskDataFromChanges
+  putTextLn $ "Copying tasks data from " <> show (length tds) <> " changes"
+  traverse_ copyTaskDataToOtherEtype (map getTds tds)
+  putTextLn "Done."
+  where
+    getTds :: BH.Hit ChangeTD -> ChangeTD
+    getTds hit = fromMaybe (error "No source") (BH.hitSource hit)
+
+    -- Return documents with tasks_data (e.g. etype == "Change")
+    getTaskDataFromChanges :: (MonadThrow m, BH.MonadBH m) => m [BH.Hit ChangeTD]
+    getTaskDataFromChanges =
+      -- simpleSearch indexName getTDsSearch
+      BH.scanSearch indexName search
+      where
+        search =
+          let -- We look for existing tasks_data
+              query = fieldExists "tasks_data"
+              -- And we only want document of type `Change`
+              filter' = BH.TermQuery (BH.Term "type" "Change") Nothing
+           in (BH.mkSearch (Just query) (Just $ BH.Filter filter'))
+                { BH.source = sourceFields ["url", "tasks_data"]
+                }
+
+    -- Copy the task data
+    copyTaskDataToOtherEtype :: (MonadThrow m, BH.MonadBH m) => ChangeTD -> m ()
+    copyTaskDataToOtherEtype ctd = do
+      notds <- getChangesWithoutTD (changeTDUrl ctd)
+      putTextLn $
+        "Going to copy task datas from "
+          <> changeTDUrl ctd
+          <> " to "
+          <> show (length notds)
+          <> " other document"
+      doBulkCopy (changeTDData ctd) (map BH.hitDocId notds)
+
+    -- Return documents matching a change url without tasks_data (e.g. etype /= "Change")
+    getChangesWithoutTD :: (MonadThrow m, BH.MonadBH m) => Text -> m [BH.Hit ChangeNoTD]
+    getChangesWithoutTD url =
+      -- simpleSearch indexName (getNoTDsSearch url)
+      BH.scanSearch indexName search
+      where
+        search =
+          let -- We look for document matching the given `url`
+              query = urlMatch url
+              -- And we only want document without a `tasks_data` field
+              filter' = BH.QueryBoolQuery $ BH.mkBoolQuery [] [] [fieldExists "tasks_data"] []
+           in (BH.mkSearch (Just query) (Just $ BH.Filter filter'))
+                { BH.source = sourceFields ["url", "type"]
+                }
+
+    -- Do the actual copy
+    doBulkCopy :: (MonadThrow m, BH.MonadBH m) => [TaskData] -> [BH.DocId] -> m ()
+    doBulkCopy _ [] = pure ()
+    doBulkCopy td docs = do
+      let updateOps = V.fromList $ map mkUpdateOp docs
+      resp <- BH.bulk updateOps
+      _ <- BH.refreshIndex indexName
+      unless (isRespOk resp) (error $ "Bulk update failed: " <> show resp)
+      where
+        mkUpdateOp doc =
+          let patch = (object ["tasks_data" .= toJSON td])
+           in BH.BulkUpdate indexName doc patch
+
+    fieldExists x = BH.QueryExistsQuery $ BH.FieldName x
+    urlMatch url = BH.TermQuery (BH.Term "url" url) Nothing
+    sourceFields xs = Just $ BH.SourceIncludeExclude (BH.Include $ map BH.Pattern xs) (BH.Exclude [])
 
 data ChangeUrl = ChangeUrl
   { changeUrl :: Text
@@ -86,17 +187,13 @@ patchUrlsDoubleSlash indexName = do
       resp <- BH.bulk updateOps
       _ <- BH.refreshIndex indexName
 
-      unless (respIsOk resp) (error $ "Bulk update failed: " <> show resp)
+      unless (isRespOk resp) (error $ "Bulk update failed: " <> show resp)
 
       -- Get more hits
       resp' <- BH.advanceScroll scrollId' 60
       case resp' of
         Right BH.SearchResult {..} -> (go (idx + 1) (BH.hits searchHits) scrollId)
         Left err -> error $ "advance scroll failed: " <> show err
-
-    respIsOk resp = case responseStatus resp of
-      Status code _ | code >= 200 && code <= 300 -> True
-      _ -> False
 
     patchUrl :: Text -> Text
     patchUrl url' =
@@ -148,14 +245,18 @@ demo server index = BH.withBH defaultManagerSettings (BH.Server server) go
       aggResp <- BH.searchByIndex testIndex aggSearch
       putTextLn (decodeUtf8 $ responseBody aggResp)
 
-    postDoc td = BH.indexDocument testIndex BH.defaultIndexDocumentSettings td (BH.DocId . show . tdId $ td)
+    postDoc td = BH.indexDocument testIndex BH.defaultIndexDocumentSettings td (BH.DocId . tdTid $ td)
 
     fakeTaskDatas :: Int -> [TaskData]
-    fakeTaskDatas x = map mkTaskData [0 .. x]
-      where
-        mkTaskData idx = TaskData idx ("https://bugzilla/show_bug.cgi?id=" <> show idx) (mkKeywords idx)
-        mkKeywords :: Int -> [Text]
-        mkKeywords idx
-          | idx `mod` 5 == 0 = ["FeatureRequest", "Patch"]
-          | idx `mod` 3 == 0 = ["Triaged"]
-          | otherwise = ["Bug"]
+    fakeTaskDatas _x = []
+
+{-
+map mkTaskData [0 .. x]
+where
+  mkTaskData idx = TaskData idx ("https://bugzilla/show_bug.cgi?id=" <> show idx) (mkKeywords idx)
+  mkKeywords :: Int -> [Text]
+  mkKeywords idx
+    | idx `mod` 5 == 0 = ["FeatureRequest", "Patch"]
+    | idx `mod` 3 == 0 = ["Triaged"]
+    | otherwise = ["Bug"]
+-}
