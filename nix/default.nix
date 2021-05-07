7e30bb20af5bbf81faa22dd06ecf21e31538321e
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-07 12:08:23 +0000

    [nix] Add missing protobuf includes

--- nix/default.nix
+++ nix/default.nix
@@ -45,6 +45,13 @@ let
     vendorSha256 = "1r0gjhv513174pbqf399vsrpx6zsmdlj48pzc5qh15k62ihy0h68";
     subPackages = [ "./apps/protoc-gen-openapi" ];
   };
+  protobuf-src = pkgs.protobuf.src;
+  googleapis-src = pkgs.fetchFromGitHub {
+    owner = "googleapis";
+    repo = "googleapis";
+    rev = "94a788e91f0f10db7d3ca38d3503d6eecefffab8";
+    sha256 = "03ky469sk0gkndxs4v8civ6x70mnnihgzaaqj51rr1xc14h40qss";
+  };
   go-req = [ gnostic ];
 
   # all requirement
@@ -54,7 +61,10 @@ in rec {
   codegen-shell = pkgs.stdenv.mkDerivation {
     name = "monocle-codegen-shell";
     buildInputs = all-req;
-    shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+    shellHook = ''
+      export PROTOC_FLAGS="-I ${googleapis-src}/ -I ${protobuf-src}/src"
+      eval $(egrep ^export ${ghc}/bin/ghc)
+    '';
   };
   # Build using: TMPDIR=/tmp/podman podman load < $(nix-build --attr codegen-container)
   codegen-container = pkgs.dockerTools.buildImage {
