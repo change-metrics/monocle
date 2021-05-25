06b1dd212fa5f8aa6d1f1a33015e33ed81703347
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-25 21:58:30 +0000

    [api] Add wai-logger

--- haskell/src/Monocle/Api/CLI.hs
+++ haskell/src/Monocle/Api/CLI.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 -- |
 module Monocle.Api.CLI (run) where
 
@@ -5,6 +8,7 @@ import qualified Monocle.Api.Config as Config
 import Monocle.Api.HTTP (MonocleAPI, server)
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
+import Network.Wai.Logger (withStdoutLogger)
 import Relude
 import Servant (serve)
 
@@ -17,4 +21,8 @@ app tenants = serve monocleAPI (server tenants)
 run :: MonadIO m => Int -> FilePath -> m ()
 run port configFile = do
   tenants <- Config.loadConfig configFile
-  liftIO $ Warp.run port (app tenants)
+  liftIO $
+    withStdoutLogger $ \aplogger -> do
+      let settings = Warp.setPort port $ Warp.setLogger aplogger Warp.defaultSettings
+      putTextLn $ "Serving " <> show (length tenants) <> " tenant(s) on 0.0.0.0:" <> show port
+      Warp.runSettings settings (app tenants)
