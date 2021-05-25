9b1ed0a9eb74b09cde19f7e176c78616fbeaf9e5
Fabien Boucher <fboucher@redhat.com>
2021-05-25 12:35:51 +0000

    Add POST endpoint sample

--- haskell/src/Monocle/WebServerApi.hs
+++ haskell/src/Monocle/WebServerApi.hs
@@ -13,6 +13,7 @@ import Servant
 type MonocleAPI =
   "indices" :> Get '[JSON] [Indice]
     :<|> "infos" :> Get '[JSON] Info
+    :<|> "query" :> ReqBody '[JSON] SimpleQueryInput :> Post '[JSON] SimpleQueryResponse
 
 data Indice = Indice
   { name :: String,
@@ -29,6 +30,21 @@ newtype Info = Info
 
 instance ToJSON Info
 
+data SimpleQueryInput = SimpleQueryInput
+  { indice :: String,
+    qs :: String
+  }
+  deriving (Eq, Show, Generic)
+
+instance FromJSON SimpleQueryInput
+
+newtype SimpleQueryResponse = SimpleQueryResponse
+  { response :: String
+  }
+  deriving (Eq, Show, Generic)
+
+instance ToJSON SimpleQueryResponse
+
 info :: Info
 info = Info "1.0.0"
 
@@ -42,6 +58,11 @@ server :: Server MonocleAPI
 server =
   return indices
     :<|> return info
+    :<|> simpleQuery
+  where
+    -- curl -vi -XPOST -H 'Accept: application/json' -H 'Content-type: application/json' -d '{"indice":"test", "qs":"my super query"}' http://localhost:8081/query
+    simpleQuery :: SimpleQueryInput -> Handler SimpleQueryResponse
+    simpleQuery inputData = return $ SimpleQueryResponse $ "Input query is: " <> qs inputData
 
 monocleAPI :: Proxy MonocleAPI
 monocleAPI = Proxy
