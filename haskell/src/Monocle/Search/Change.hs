bca42c5d823809e257ba32d19db3331383f5f64e
Fabien Boucher <fboucher@redhat.com>
2021-05-27 15:25:39 +0000

    [api-ng] Add full Change datatype

--- haskell/src/Monocle/Search/Change.hs
+++ haskell/src/Monocle/Search/Change.hs
@@ -20,14 +20,88 @@ instance ToJSON Author where
 instance FromJSON Author where
   parseJSON = genericParseJSON $ aesonPrefix snakeCase
 
+data File = File
+  { fileAdditions :: Int,
+    fileDeletions :: Int,
+    filePath :: Text
+  }
+  deriving (Show, Eq, Generic)
+
+instance ToJSON File where
+  toJSON = genericToJSON $ aesonPrefix snakeCase
+
+instance FromJSON File where
+  parseJSON = genericParseJSON $ aesonPrefix snakeCase
+
+data Commit = Commit
+  { commitSha :: Text,
+    commitAuthor :: Author,
+    commitCommitter :: Author,
+    commitAuthoredAt :: UTCTime,
+    commitCommittedAt :: UTCTime,
+    commitAdditions :: Int,
+    commitDeletions :: Int,
+    commitTitle :: Text
+  }
+  deriving (Show, Eq, Generic)
+
+instance ToJSON Commit where
+  toJSON = genericToJSON $ aesonPrefix snakeCase
+
+instance FromJSON Commit where
+  parseJSON = genericParseJSON $ aesonPrefix snakeCase
+
+data TaskData = TaskData
+  { tdTid :: Text,
+    tdTtype :: Text,
+    tdUpdatedAt :: UTCTime,
+    tdChangeUrl :: Text,
+    tdSeverity :: Text,
+    tdPriority :: Text,
+    tdScore :: Int,
+    tdUrl :: Text,
+    tdTitle :: Text
+  }
+  deriving (Show, Eq, Generic)
+
+instance ToJSON TaskData where
+  toJSON = genericToJSON $ aesonPrefix snakeCase
+
+instance FromJSON TaskData where
+  parseJSON = genericParseJSON $ aesonPrefix snakeCase
+
 data Change = Change
-  { changeState :: Text,
-    changeUrl :: Text,
-    changeCreatedAt :: UTCTime,
-    changeUpdatedAt :: UTCTime,
+  { changeNumber :: Int,
+    changeChangeId :: Text,
     changeTitle :: Text,
+    changeText :: Text,
+    changeUrl :: Text,
+    changeCommitCount :: Int,
+    changeAdditions :: Int,
+    changeDeletions :: Int,
+    changeChangedFilesCount :: Int,
+    changeChangedFiles :: [File],
+    changeCommits :: [Commit],
+    changeRepositoryPrefix :: Text,
+    changeRepositoryShortname :: Text,
     changeRepositoryFullname :: Text,
-    changeAuthor :: Author
+    changeAuthor :: Author,
+    changeCommitter :: Maybe Author,
+    changeMergedBy :: Maybe Author,
+    changeBranch :: Text,
+    changeCreatedAt :: UTCTime,
+    changeMergedAt :: Maybe UTCTime,
+    changeUpdatedAt :: UTCTime,
+    changeClosedAt :: Maybe UTCTime,
+    changeState :: Text,
+    changeDuration :: Maybe Int,
+    changeMergeable :: Text,
+    changeLabels :: [Text],
+    changeAssignees :: [Author],
+    changeApproval :: Maybe [Text],
+    changeDraft :: Bool,
+    changeSelfMerged :: Maybe Bool,
+    changeTasksData :: Maybe [TaskData]
   }
   deriving (Show, Eq, Generic)
 
