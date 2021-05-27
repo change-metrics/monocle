c6ef68c4f141ff87e90b3b4ef190154f82c4b901
Tristan Cacqueray <tdecacqu@redhat.com>
2021-05-27 19:50:40 +0000

    [api-ng] Add debug comment

--- haskell/src/Monocle/Search/Queries.hs
+++ haskell/src/Monocle/Search/Queries.hs
@@ -6,7 +6,7 @@
 module Monocle.Search.Queries where
 
 import Control.Monad.Catch (MonadThrow)
-import Data.Aeson (FromJSON (..))
+import qualified Data.Aeson as Aeson
 import qualified Database.Bloodhound as BH
 import Monocle.Search.Change (ELKChange (..))
 import qualified Monocle.Search.Query as Q
@@ -20,8 +20,9 @@ mkEnv server = do
   pure $ BH.mkBHEnv (BH.Server server) manager
 
 -- | Helper search func that can be replaced by a scanSearch
-simpleSearch :: (FromJSON a, MonadThrow m, BH.MonadBH m) => BH.IndexName -> BH.Search -> m [BH.Hit a]
+simpleSearch :: (Aeson.FromJSON a, MonadThrow m, BH.MonadBH m) => BH.IndexName -> BH.Search -> m [BH.Hit a]
 simpleSearch indexName search = do
+  -- putTextLn . decodeUtf8 . Aeson.encode $ search
   rawResp <- BH.searchByIndex indexName search
   resp <- BH.parseEsResponse rawResp
   case resp of
