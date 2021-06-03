cbb70594d671ebf3d80f812216543abf60801967
Fabien Boucher <fboucher@redhat.com>
2021-06-03 10:41:08 +0000

    Fill Monocle.Change from ProjectMergeRequestsNodesMergeRequest Morpheus type

--- lentille-gitlab/src/Lentille/GitLab/MergeRequests.hs
+++ lentille-gitlab/src/Lentille/GitLab/MergeRequests.hs
@@ -39,11 +39,38 @@ defineByDocumentFile
   [gql|
     query GetProjectMergeRequests ($project: ID!, $cursor: String) {
       project(fullPath: $project) {
+        name
+        nameWithNamespace
+        namespace {
+          name
+        }
         mergeRequests (first: 100, after: $cursor, sort: UPDATED_DESC) {
           pageInfo {hasNextPage endCursor}
           count
           nodes {
+            id
             title
+            description
+            webUrl
+            commitCount
+            diffStatsSummary {
+              additions
+              deletions
+              fileCount
+            }
+            commitsWithoutMergeCommits (first: 100) {
+              nodes {
+                sha
+                author {
+                  username
+                }
+                authoredDate
+                title
+              }
+            }
+            author {
+              username
+            }
             updatedAt
           }
         }
@@ -73,6 +100,9 @@ transformResponse result =
     GetProjectMergeRequests
       ( Just
           ( ProjectProject
+              shortName
+              fullName
+              nameSpaceM
               ( Just
                   ( ProjectMergeRequestsMergeRequestConnection
                       (ProjectMergeRequestsPageInfoPageInfo hasNextPage endCursor)
@@ -81,41 +111,49 @@ transformResponse result =
                     )
                 )
             )
-        ) -> (PageInfo hasNextPage endCursor count, [], nodesToChanges nodes)
+        ) ->
+        ( PageInfo hasNextPage endCursor count,
+          [],
+          (nodesToChanges shortName fullName (toNamespaceName nameSpaceM)) nodes
+        )
     otherWise -> error ("Invalid response: " <> show otherwise)
   where
-    nodesToChanges :: [Maybe ProjectMergeRequestsNodesMergeRequest] -> [Change]
-    nodesToChanges nodes' =
+    toNamespaceName nsoM = case nsoM of
+      Just nso -> name (nso :: ProjectNamespaceNamespace)
+      Nothing -> ""
+    fromMTtoLT t = toLazy $ fromMaybe "" t
+    fromIntToInt32 = fromInteger . toInteger
+    nodesToChanges :: Text -> Text -> Text -> [Maybe ProjectMergeRequestsNodesMergeRequest] -> [Change]
+    nodesToChanges shortName fullName namespace nodes' =
       map toChange (catMaybes nodes')
       where
         toChange :: ProjectMergeRequestsNodesMergeRequest -> Change
         toChange mr =
           Change
-            "Id"
+            (let ID id' = Lentille.GitLab.MergeRequests.id mr in show id')
             1
             "ChangeId"
-            (toLazy $ title mr)
-            "Text"
-            "URL"
-            1
-            0
-            0
-            0
-            changedFiles
-            commits
-            "rp"
-            "rf"
-            "rs"
-            (Just fakeIdent)
-            (Just fakeIdent)
-            (Just fakeIdent)
+            (toLazy $ title (mr :: ProjectMergeRequestsNodesMergeRequest))
+            (fromMTtoLT $ description mr)
+            (fromMTtoLT $ webUrl mr)
+            (fromIntToInt32 . fromMaybe 0 $ commitCount mr)
+            (getAdditions $ diffStatsSummary mr)
+            (getDeletions $ diffStatsSummary mr)
+            (getChangedFileCount $ diffStatsSummary mr)
+            -- Unable to find a solution for the list of changed files
+            empty
+            (getCommits $ commitsWithoutMergeCommits mr)
+            (toLazy namespace)
+            (toLazy fullName)
+            (toLazy shortName)
+            (Just $ getAuthorIdent mr)
             Nothing
             "my-branch"
             "master"
             Nothing
             Nothing
             Nothing
-            (Just $ updatedAtToTimestamp $ updatedAt mr)
+            (Just $ timeToTimestamp $ updatedAt mr)
             Nothing
             "merged"
             60
@@ -125,10 +163,56 @@ transformResponse result =
             approvals
             False
             Nothing
-        changedFiles :: V.Vector ChangedFile
-        changedFiles = empty
-        commits :: V.Vector Commit
-        commits = empty
+        getAuthorIdent :: ProjectMergeRequestsNodesMergeRequest -> Ident
+        getAuthorIdent mr = toIdent author (mr :: ProjectMergeRequestsNodesMergeRequest)
+        getAdditions :: Maybe ProjectMergeRequestsNodesDiffStatsSummaryDiffStatsSummary -> Int32
+        getAdditions diffStatsM =
+          case diffStatsM of
+            Just diffStats -> fromIntToInt32 $ additions diffStats
+            Nothing -> 0
+        getDeletions diffStatsM =
+          case diffStatsM of
+            Just diffStats -> fromIntToInt32 $ deletions diffStats
+            Nothing -> 0
+        getChangedFileCount diffStatsM =
+          case diffStatsM of
+            Just diffStats -> fromIntToInt32 $ fileCount diffStats
+            Nothing -> 0
+        getCommits :: Maybe ProjectMergeRequestsNodesCommitsWithoutMergeCommitsCommitConnection -> V.Vector Commit
+        getCommits commitsM =
+          case commitsM of
+            Just
+              ( ProjectMergeRequestsNodesCommitsWithoutMergeCommitsCommitConnection
+                  (Just commits)
+                ) -> fromList $ map toCommit (catMaybes commits)
+            _ -> empty
+          where
+            toCommit :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit -> Commit
+            toCommit c =
+              -- Unable to get the committer and the committed_at date
+              -- Unable to get additions/deletions
+              Commit
+                (toLazy $ sha c)
+                (Just . toIdent . getUsername $ author (c :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit))
+                (Just . toIdent . getUsername $ author (c :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit))
+                (Just . timeToTimestamp $ fromMaybe defaultTimestamp $ authoredDate c)
+                (Just . timeToTimestamp $ fromMaybe defaultTimestamp $ authoredDate c)
+                0
+                0
+                ( toLazy
+                    ( fromMaybe
+                        ""
+                        ( title (c :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit)
+                        )
+                    )
+                )
+            getUsername :: Maybe ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesAuthorUserCore -> Text
+            getUsername uM = case uM of
+              Just u -> username (u :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesAuthorUserCore)
+              Nothing -> ""
+            defaultTimestamp = Time "1970-01-01T00:00:00Z"
+        toIdent :: Text -> Ident
+        toIdent username' = Ident (toLazy $ "gitlab.com" <> "/" <> username') (toLazy username')
         assignees :: V.Vector Ident
         assignees = empty
         labels :: V.Vector LText
@@ -137,6 +221,6 @@ transformResponse result =
         approvals = empty
         fakeIdent :: Ident
         fakeIdent = Ident "fabien" "fabien"
-        updatedAtToTimestamp :: Time -> T.Timestamp
-        updatedAtToTimestamp t =
+        timeToTimestamp :: Time -> T.Timestamp
+        timeToTimestamp t =
           let Time tt = t in T.fromUTCTime $ parseTimeOrError False defaultTimeLocale "%FT%XZ" $ toString tt
