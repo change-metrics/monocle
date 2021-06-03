3af2e22269ec2d66d39f4ec504d4f9daf1ed8747
Fabien Boucher <fboucher@redhat.com>
2021-06-03 14:12:22 +0000

    More fields

--- lentille-gitlab/src/Lentille/GitLab/MergeRequests.hs
+++ lentille-gitlab/src/Lentille/GitLab/MergeRequests.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
@@ -28,10 +29,14 @@ import Lentille.GitLab
     streamFetch,
   )
 import Monocle.Change
-import Relude
+import Relude hiding (id)
 import Streaming (Of, Stream)
 
+-- TODO(fbo): use NamedFieldPuns
+
 newtype Time = Time Text deriving (Show, Eq, EncodeScalar, DecodeScalar)
+
+data MergeRequestStateAlt = MergeRequestStateOpened | MergeRequestStateClosed | MergeRequestStateLocked | MergeRequestStateAll | MergeRequestStateMerged
 
 -- https://docs.gitlab.com/ee/api/graphql/reference/index.html#projectmergerequests
 defineByDocumentFile
@@ -71,7 +76,14 @@ defineByDocumentFile
             author {
               username
             }
+            mergeUser {
+              username
+            }
+            sourceBranch
+            targetBranch
+            createdAt
             updatedAt
+            mergedAt
           }
         }
       }
@@ -147,13 +159,13 @@ transformResponse result =
             (toLazy fullName)
             (toLazy shortName)
             (Just $ getAuthorIdent mr)
-            Nothing
-            "my-branch"
-            "master"
-            Nothing
-            Nothing
-            Nothing
+            (Just . ChangeOptionalMergedByMergedBy $ getMergedByIdent mr)
+            (toLazy $ sourceBranch mr)
+            (toLazy $ targetBranch mr)
+            (Just $ timeToTimestamp $ createdAt mr)
+            (getMergedAt $ mergedAt mr)
             (Just $ timeToTimestamp $ updatedAt mr)
+            -- No closedAt attribute for a MR ?
             Nothing
             "merged"
             60
@@ -163,21 +175,29 @@ transformResponse result =
             approvals
             False
             Nothing
+        getMergedAt :: Maybe Time -> Maybe ChangeOptionalMergedAt
+        getMergedAt tM = fmap (ChangeOptionalMergedAtMergedAt . timeToTimestamp) tM
+        ghostIdent = Ident "ghost" "ghost"
         getAuthorIdent :: ProjectMergeRequestsNodesMergeRequest -> Ident
-        getAuthorIdent mr = toIdent author (mr :: ProjectMergeRequestsNodesMergeRequest)
-        getAdditions :: Maybe ProjectMergeRequestsNodesDiffStatsSummaryDiffStatsSummary -> Int32
-        getAdditions diffStatsM =
+        getAuthorIdent ProjectMergeRequestsNodesMergeRequest {..} = case author of
+          Just author' -> toIdent $ getUsername author'
+          Nothing -> ghostIdent
+          where
+            getUsername ProjectMergeRequestsNodesAuthorUserCore {..} = username
+        getMergedByIdent :: ProjectMergeRequestsNodesMergeRequest -> Ident
+        getMergedByIdent ProjectMergeRequestsNodesMergeRequest {..} = case mergeUser of
+          Just author' -> toIdent $ getUsername author'
+          Nothing -> ghostIdent
+          where
+            getUsername ProjectMergeRequestsNodesMergeUserUserCore {..} = username
+        getDiffStatsAttr :: Maybe ProjectMergeRequestsNodesDiffStatsSummaryDiffStatsSummary -> (ProjectMergeRequestsNodesDiffStatsSummaryDiffStatsSummary -> Int) -> Int32
+        getDiffStatsAttr diffStatsM cb =
           case diffStatsM of
-            Just diffStats -> fromIntToInt32 $ additions diffStats
+            Just diffStats -> fromIntToInt32 $ cb diffStats
             Nothing -> 0
-        getDeletions diffStatsM =
-          case diffStatsM of
-            Just diffStats -> fromIntToInt32 $ deletions diffStats
-            Nothing -> 0
-        getChangedFileCount diffStatsM =
-          case diffStatsM of
-            Just diffStats -> fromIntToInt32 $ fileCount diffStats
-            Nothing -> 0
+        getAdditions diffStatsM = getDiffStatsAttr diffStatsM additions
+        getDeletions diffStatsM = getDiffStatsAttr diffStatsM deletions
+        getChangedFileCount diffStatsM = getDiffStatsAttr diffStatsM fileCount
         getCommits :: Maybe ProjectMergeRequestsNodesCommitsWithoutMergeCommitsCommitConnection -> V.Vector Commit
         getCommits commitsM =
           case commitsM of
@@ -188,24 +208,18 @@ transformResponse result =
             _ -> empty
           where
             toCommit :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit -> Commit
-            toCommit c =
+            toCommit ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit {..} =
               -- Unable to get the committer and the committed_at date
               -- Unable to get additions/deletions
               Commit
-                (toLazy $ sha c)
-                (Just . toIdent . getUsername $ author (c :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit))
-                (Just . toIdent . getUsername $ author (c :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit))
-                (Just . timeToTimestamp $ fromMaybe defaultTimestamp $ authoredDate c)
-                (Just . timeToTimestamp $ fromMaybe defaultTimestamp $ authoredDate c)
+                (toLazy sha)
+                (Just . toIdent . getUsername $ author)
+                (Just . toIdent . getUsername $ author)
+                (Just . timeToTimestamp $ fromMaybe defaultTimestamp authoredDate)
+                (Just . timeToTimestamp $ fromMaybe defaultTimestamp authoredDate)
                 0
                 0
-                ( toLazy
-                    ( fromMaybe
-                        ""
-                        ( title (c :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesCommit)
-                        )
-                    )
-                )
+                (toLazy $ fromMaybe "" title)
             getUsername :: Maybe ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesAuthorUserCore -> Text
             getUsername uM = case uM of
               Just u -> username (u :: ProjectMergeRequestsNodesCommitsWithoutMergeCommitsNodesAuthorUserCore)
@@ -219,8 +233,6 @@ transformResponse result =
         labels = empty
         approvals :: V.Vector LText
         approvals = empty
-        fakeIdent :: Ident
-        fakeIdent = Ident "fabien" "fabien"
         timeToTimestamp :: Time -> T.Timestamp
         timeToTimestamp t =
           let Time tt = t in T.fromUTCTime $ parseTimeOrError False defaultTimeLocale "%FT%XZ" $ toString tt
