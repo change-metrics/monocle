ba49b16acbf884df8c7f1f9ff21fa71c2c4eb7c4
Fabien Boucher <fboucher@redhat.com>
2021-10-20 16:08:46 +0000

    monocle-api app - use optparse-generic for args parser

--- haskell/app/Api.hs
+++ haskell/app/Api.hs
@@ -1,17 +1,46 @@
--- | The monocle-api entrypoint
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 module Main (main) where
 
-import Data.Text.IO (hPutStrLn)
 import qualified Monocle.Api
+import Options.Generic
 import Relude
+
+data CLIOptions w = CLIOption
+  { elasticUrl :: w ::: Maybe String <?> "The Elasticsearch endpoint (or ELASTIC_CONN env), default to elastic:9200",
+    config :: w ::: Maybe FilePath <?> "The Monocle configuration (or CONFIG env), default to /etc/monocle/config.yaml",
+    port :: w ::: Maybe String <?> "The API port to listen to (or PORT env), default to 9898"
+  }
+  deriving stock (Generic)
+
+instance ParseRecord (CLIOptions Wrapped) where
+  parseRecord = parseRecordWithModifiers lispCaseModifiers
+
+getOptWithDefault :: Maybe String -> String -> String -> IO String
+getOptWithDefault argOpt envName defaultValue = do
+  fromEnv <- lookupEnv envName
+  pure $ fromMaybe (fromMaybe defaultValue fromEnv) argOpt
+
+getConfigOpt :: Maybe String -> IO String
+getConfigOpt argOpt = getOptWithDefault argOpt "CONFIG" "/etc/monocle/config.yaml"
+
+getURLOpt :: Maybe String -> IO Text
+getURLOpt argOpt = do
+  url <- getOptWithDefault argOpt "ELASTIC_CONN" "elastic:9200"
+  let hasScheme = isPrefixOf "http://" url || isPrefixOf "https://" url
+      url' = if hasScheme then url else "http://" <> url
+  pure $ toText url'
+
+getPortOpt :: Maybe String -> IO Int
+getPortOpt argOpt = do
+  port <- getOptWithDefault argOpt "PORT" "9898"
+  pure $ fromMaybe (error "Invalid port") (readMaybe port)
 
 main :: IO ()
 main = do
-  args <- getArgs
-  config <- fromMaybe "/etc/monocle/config.yaml" <$> lookupEnv "CONFIG"
-  url <- fromMaybe "elastic:9200" <$> lookupEnv "ELASTIC_CONN"
-  case args of
-    ["--port", portStr] ->
-      let port = fromMaybe (error $ "Could not read port: " <> toText portStr) $ readMaybe portStr
-       in Monocle.Api.run port ("http://" <> toText url) config
-    _ -> hPutStrLn stderr "usage: monocle-api --port PORT"
+  args <- unwrapRecord "Monocle API"
+  config <- getConfigOpt (config args)
+  url <- getURLOpt (elasticUrl args)
+  port <- getPortOpt (port args)
+  Monocle.Api.run port url config
