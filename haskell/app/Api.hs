ba723370232cc49b7b4584ce2e41cc339ec82ecd
Fabien Boucher <fboucher@redhat.com>
2021-10-21 10:08:25 +0000

    monocle-cli: handle environment variables via envparse package

--- haskell/app/Api.hs
+++ haskell/app/Api.hs
@@ -1,46 +1,57 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Main (main) where
 
+import Env
 import qualified Monocle.Api
 import Options.Generic
 import Relude
 
+---------------------------------------------------------------
+-- CLI available environment variables
+---------------------------------------------------------------
+
+data CliEnv = CliEnv
+  { config :: String,
+    elastic_conn :: String
+  }
+
+cliEnv :: IO CliEnv
+cliEnv =
+  Env.parse (header "monocle-api available environement variables") $
+    CliEnv <$> var (str) "CONFIG" (help "The Monocle configuration" <> def "/etc/monocle/config.yaml" <> helpDef show)
+      <*> var (str) "ELASTIC_CONN" (help "The Elasticsearch endpoint" <> def "elastic:9200")
+
+---------------------------------------------------------------
+-- CLI arguments parser
+---------------------------------------------------------------
+
 data CLIOptions w = CLIOption
-  { elasticUrl :: w ::: Maybe String <?> "The Elasticsearch endpoint (or ELASTIC_CONN env), default to elastic:9200",
-    config :: w ::: Maybe FilePath <?> "The Monocle configuration (or CONFIG env), default to /etc/monocle/config.yaml",
-    port :: w ::: Maybe String <?> "The API port to listen to (or PORT env), default to 9898"
+  { port :: w ::: Maybe Int <?> "The API port to listen to, default to 9898"
   }
   deriving stock (Generic)
 
 instance ParseRecord (CLIOptions Wrapped) where
   parseRecord = parseRecordWithModifiers lispCaseModifiers
 
-getOptWithDefault :: Maybe String -> String -> String -> IO String
-getOptWithDefault argOpt envName defaultValue = do
-  fromEnv <- lookupEnv envName
-  pure $ fromMaybe (fromMaybe defaultValue fromEnv) argOpt
+---------------------------------------------------------------
+-- CLI functions
+---------------------------------------------------------------
 
-getConfigOpt :: Maybe String -> IO String
-getConfigOpt argOpt = getOptWithDefault argOpt "CONFIG" "/etc/monocle/config.yaml"
-
-getURLOpt :: Maybe String -> IO Text
-getURLOpt argOpt = do
-  url <- getOptWithDefault argOpt "ELASTIC_CONN" "elastic:9200"
+getURL :: String -> Text
+getURL url =
   let hasScheme = isPrefixOf "http://" url || isPrefixOf "https://" url
       url' = if hasScheme then url else "http://" <> url
-  pure $ toText url'
-
-getPortOpt :: Maybe String -> IO Int
-getPortOpt argOpt = do
-  port <- getOptWithDefault argOpt "PORT" "9898"
-  pure $ fromMaybe (error "Invalid port") (readMaybe port)
+   in toText url'
 
 main :: IO ()
 main = do
+  -- Fetch environement variables
+  CliEnv {config, elastic_conn} <- cliEnv
+  -- Run arguments parser
   args <- unwrapRecord "Monocle API"
-  config <- getConfigOpt (config args)
-  url <- getURLOpt (elasticUrl args)
-  port <- getPortOpt (port args)
-  Monocle.Api.run port url config
+  -- Run the Monocle API
+  Monocle.Api.run (fromMaybe 8989 (port args)) (getURL elastic_conn) config
