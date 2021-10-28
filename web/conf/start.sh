57170de3e01c4474dc161dbeca732a4c399c0b92
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-10-28 20:26:13 +0300

    feat(web): force deployment to work only on deployment
    the 'server' directive doesn't work on a pod that doesn't have access to port 80
    the 'user' only works if the current PID is root, which the container is not
    
    changing these made the pod run and expose the web endpoint

--- web/conf/start.sh
+++ web/conf/start.sh
@@ -6,5 +6,15 @@
 test -z "$REACT_APP_API_URL" || (sed  -e "s@__API_URL__@$REACT_APP_API_URL@" /app/index.html > /tmp/index.html; cp /tmp/index.html /app/index.html; rm /tmp/index.html);
 test -z "$REACT_APP_TITLE"   || (sed -i -e "s@__TITLE__@$REACT_APP_TITLE@" /app/index.html > /tmp/index.html; cp /tmp/index.html /app/index.html; rm /tmp/index.html);
 
+# remove the default server config
+sed '37,54d' /etc/nginx/nginx.conf > /tmp/nginx.conf
+cp /tmp/nginx.conf /etc/nginx/nginx.conf
+rm /tmp/nginx.conf
+
+sed '/^user nginx;$/d' /etc/nginx/nginx.conf > /tmp/nginx.conf
+cp /tmp/nginx.conf /etc/nginx/nginx.conf
+rm /tmp/nginx.conf
+
+
 # Start the web server
 exec nginx -g "daemon off;"
