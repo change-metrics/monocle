00ab58b17ab9bbec78b7fa46fa99c87763f627ac
Tristan Cacqueray <tdecacqu@redhat.com>
2021-11-04 15:47:22 +0000

    [backend] Move all the CLI code to a single module
    This change finishes the migration to a god library where
    the common code is now part of the CLI module.

--- haskell/app/Api.hs
+++ haskell/app/Api.hs
@@ -1,57 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-
 module Main (main) where
 
-import Env
-import qualified Monocle.Api
-import Options.Generic
-import Relude
-
----------------------------------------------------------------
--- CLI available environment variables
----------------------------------------------------------------
-
-data CliEnv = CliEnv
-  { config :: String,
-    elastic_conn :: String
-  }
-
-cliEnv :: IO CliEnv
-cliEnv =
-  Env.parse (header "monocle-api available environement variables") $
-    CliEnv <$> var (str) "CONFIG" (help "The Monocle configuration" <> def "/etc/monocle/config.yaml" <> helpDef show)
-      <*> var (str) "ELASTIC_CONN" (help "The Elasticsearch endpoint" <> def "elastic:9200")
-
----------------------------------------------------------------
--- CLI arguments parser
----------------------------------------------------------------
-
-data CLIOptions w = CLIOption
-  { port :: w ::: Maybe Int <?> "The API port to listen to, default to 9898"
-  }
-  deriving stock (Generic)
-
-instance ParseRecord (CLIOptions Wrapped) where
-  parseRecord = parseRecordWithModifiers lispCaseModifiers
-
----------------------------------------------------------------
--- CLI functions
----------------------------------------------------------------
-
-getURL :: String -> Text
-getURL url =
-  let hasScheme = isPrefixOf "http://" url || isPrefixOf "https://" url
-      url' = if hasScheme then url else "http://" <> url
-   in toText url'
+import qualified CLI
+import Prelude (IO)
 
 main :: IO ()
-main = do
-  -- Fetch environement variables
-  CliEnv {config, elastic_conn} <- cliEnv
-  -- Run arguments parser
-  args <- unwrapRecord "Monocle API"
-  -- Run the Monocle API
-  Monocle.Api.run (fromMaybe 8989 (port args)) (getURL elastic_conn) config
+main = CLI.mainApi
--- haskell/app/Macroscope.hs
+++ haskell/app/Macroscope.hs
@@ -1,29 +1,7 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleInstances #-}
-
 module Main (main) where
 
-import Macroscope.Main (runMacroscope)
-import Monocle.Client (withClient)
-import Options.Generic
-import Relude
-
-data Macroscope w = Macroscope
-  { monocleUrl :: w ::: Maybe Text <?> "The monocle API",
-    port :: w ::: Int <!> "9001" <?> "Health check port",
-    config :: w ::: Maybe FilePath <?> "The monocle configuration"
-  }
-  deriving stock (Generic)
-
-instance ParseRecord (Macroscope Wrapped) where
-  parseRecord = parseRecordWithModifiers lispCaseModifiers
+import qualified CLI
+import Prelude (IO)
 
 main :: IO ()
-main = do
-  args <- unwrapRecord "Macroscope lentille runner"
-  config' <- fromMaybe (error "--config or CONFIG env is required") <$> lookupEnv "CONFIG"
-  withClient (fromMaybe "http://web:8080" $ monocleUrl args) Nothing $ \client ->
-    runMacroscope
-      (port args)
-      (fromMaybe config' $ config args)
-      client
+main = CLI.mainMacroscope
