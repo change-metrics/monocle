7072c08c5e7bc31f672650f1b203a513349bc82b
Fabien Boucher <fboucher@redhat.com>
2022-05-23 14:26:31 +0000

    Monocle.Api.Jwt: add a function to generate a JSON Web Token

---
+++ haskell/src/Monocle/Api/Jwt.hs
@@ -0,0 +1,50 @@
+module Monocle.Api.Jwt
+  ( mkMagicJwt,
+  )
+where
+
+import Crypto.JWT
+  ( ClaimsSet,
+    JWK,
+    JWTError,
+    KeyMaterialGenParam (RSAGenParam),
+    NumericDate (NumericDate),
+    SignedJWT,
+    StringOrURI,
+    bestJWSAlg,
+    claimIat,
+    claimIss,
+    claimSub,
+    emptyClaimsSet,
+    genJWK,
+    newJWSHeader,
+    signClaims,
+  )
+import Monocle.Prelude
+  ( getCurrentTime,
+    (?~),
+  )
+import Relude
+
+mkMagicClaims :: MonadIO m => StringOrURI -> m ClaimsSet
+mkMagicClaims subject = do
+  t <- getCurrentTime
+  pure $
+    emptyClaimsSet
+      & claimIss ?~ "MagicJWT"
+      & claimSub ?~ subject
+      & claimIat ?~ NumericDate t
+
+doSignJwt :: JWK -> ClaimsSet -> IO (Either JWTError SignedJWT)
+doSignJwt jwk claims = runExceptT $ do
+  alg <- bestJWSAlg jwk
+  signClaims jwk (newJWSHeader ((), alg)) claims
+
+doGenJwk :: IO JWK
+doGenJwk = genJWK (RSAGenParam (4096 `div` 8))
+
+mkMagicJwt :: String -> IO (Either JWTError SignedJWT)
+mkMagicJwt subject = do
+  claims <- mkMagicClaims $ fromString subject
+  jwk <- doGenJwk
+  doSignJwt jwk claims
